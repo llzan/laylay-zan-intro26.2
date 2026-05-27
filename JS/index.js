0f17d7f382000a2c8d51898f15e6560b2dd52529
Lay Lay Zan <132317800+llzan@users.noreply.github.com>
2026-05-26 22:09:30 -0500

    week 7 revisions

--- JS/index.js
+++ JS/index.js
@@ -2,16 +2,16 @@ let today = new Date();
 
 let thisYear = today.getFullYear();
 
-let footer = document.querySelector("footer");
-
+let footer = document.createElement("footer");
 let copyright = document.createElement("p");
 
 copyright.innerHTML = `&copy; ${thisYear} Lay Lay Zan`;
 
 footer.appendChild(copyright);
+document.body.appendChild(footer);
 
 let skills = ["HTML", "CSS", "JavaScript", "UX Design", "Graphic Design"];
-let skillsList = document.querySelector("#Skills ul");
+let skillsList = document.querySelector("#skills ul");
 
 for (let i = 0; i < skills.length; i++) {
   let skill = document.createElement("li");
@@ -20,7 +20,7 @@ for (let i = 0; i < skills.length; i++) {
 }
 
 let projects = ["LSAT Study Guide", "Earthday Login", "Meal Planner App"];
-let projectsList = document.querySelector("#Projects ul");
+let projectsList = document.querySelector("#projects ul");
 
 for (let i = 0; i < projects.length; i++) {
   let project = document.createElement("li");
