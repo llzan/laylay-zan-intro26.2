b7f13ee44b622cd178e409fe5e260b555871dded
Lay Lay Zan <132317800+llzan@users.noreply.github.com>
2026-06-03 00:02:54 -0500

    API fetch completed

--- JS/index.js
+++ JS/index.js
@@ -27,3 +27,58 @@ for (let i = 0; i < projects.length; i++) {
   project.textContent = projects[i];
   projectsList.appendChild(project);
 }
+
+messageForm = document.forms["leave_message"];
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let name = event.target.usersName["name"].value;
+  let email = event.target.usersEmail["email"].value;
+  let message = event.target.usersMessage["message"].value;
+  console.log(name, email, message);
+
+  let messageSectiuon = document.querySelector("#messages ul");
+  let messageList = document.querySelector("#messages li");
+  let newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:${email}">${usersName}</a> <span>(${usersEmail})</span> says: <span>${usersMessage}</span>`;
+  messageSectiuon.appendChild(newMessage);
+
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.type = "button";
+  newMessage.appendChild(removeButton);
+
+  removeButton.addEventListener("click", function () {
+    messageSectiuon.removeChild(newMessage);
+
+    let entry = entry.parentNode;
+    entry.parentNode.removeChild(entry);
+  });
+
+  document.querySelector("#messages ul").removeChild(newMessage);
+  document.querySelector("#messages li").removeChild(messageList);
+
+  messageForm.reset();
+});
+
+const api_url = "https://api.github.com/users/laylayzan/repos";
+
+const projectSection = document.querySelector("#projects ul");
+console.log(projectSection);
+
+const projectList = projectSection.querySelectorAll("li");
+console.log(projectList);
+
+fetch(api_url)
+  .then((response) => response.json())
+  .then((data) => {
+    let projectSection = document.querySelector("#projects ul");
+    data.forEach((repo) => {
+      let project = document.createElement("li");
+      let link = document.createElement("a");
+      link.textContent = repo.name;
+      project.appendChild(link);
+      projectSection.appendChild(project);
+    });
+  })
+  .catch((error) => console.error(error));
